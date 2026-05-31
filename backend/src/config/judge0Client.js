b338faff118be28971a6f0f63c639e1475d5734d
Lokeshkumar719 <lokesh6182005@gmail.com>
2026-05-31 16:44:38 +0530

    judge0 timeout bug fixed

--- backend/src/config/judge0Client.js
+++ backend/src/config/judge0Client.js
@@ -8,7 +8,7 @@ const judge0Client = axios.create({
     "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
     "Content-Type": "application/json",
   },
-  timeout: 10000,
+  timeout: 60000,
 });
 
 module.exports = judge0Client;
